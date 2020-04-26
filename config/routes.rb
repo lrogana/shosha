e3f01eec9adccbc4cbe1ca48cd022662f40dab43
Liston Ogana <liston@uber.com>
2020-04-26 06:54:08 -0600

    basic login

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  root 'application#hello'
+  #root 'application#hello'
+  root   'sessions#new'
   get  '/signup',  to: 'users#new'
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
   resources :users
 end
