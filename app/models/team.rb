cd6759d6eecaf207821c7bf432bd3ddd624cca53
liston <2ndcenter@gmail.com>
2020-05-28 10:12:54 -0600

    Added a home/landing page and added signup button on the login page

--- app/models/team.rb
+++ app/models/team.rb
@@ -2,8 +2,9 @@ class Team < ApplicationRecord
   belongs_to :tournament
   belongs_to :user
   validates :tournament_id, presence: true
-  validates :name, presence:true
-  validates :team_contact_name, presence: true
-  validates :team_contact_phone_number, presence: true
+  validates :name,  presence: true, length: { maximum: 50 }
+  validates :team_contact_name, presence: true, length: { maximum: 50 }
+  validates :team_contact_phone_number, presence: true, length: { maximum: 20 }
+
   default_scope -> { order(created_at: :desc) }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
+  get 'static_pages/home'
   resources :teams
   resources :tournaments
   get 'sessions/new'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   #root 'application#hello'
-  root   'tournaments#index'
+  #root   'tournaments#index'
+  root 'static_pages#home'
   get  '/signup',  to: 'users#new'
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
